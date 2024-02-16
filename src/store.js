fa1aa87e1bac652f99a5595b60488100de4a02c7
marcobusacca <marcobusacca01@gmail.com>
2024-02-16 18:17:18 +0100

    [FEAT] Configurazione store.js con "baseUrl" e "loading" per Chiamate API

---
+++ src/store.js
@@ -0,0 +1,8 @@
+// IMPORTO REACTIVE DA VUE
+import { reactive } from "vue";
+
+// ESPORTO LA CONSTANTE STORE CONTENENTE TUTTI GLI ELEMENTI ACCESSIBILI AD OGNI COMPONENTE VUE
+export const store = reactive({
+    baseUrl: 'http://127.0.0.1:8080',
+    loading: false,
+});
